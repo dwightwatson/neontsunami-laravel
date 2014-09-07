ec33c9de7008b73cbbe25084613e870fd127cbd2
Dwight Watson <dwightwatson@me.com>
2014-09-07 13:07:51 +1000

    Increase the amount of content for projects in a list

--- app/views/projects/_project.blade.php
+++ app/views/projects/_project.blade.php
@@ -1,4 +1,4 @@
 <div class="project">
   <h4 class="project-name">{{ link_to_route('projects.show', $project->name, $project->slug) }}</h4>
-  {{ markdown(Str::limit($project->description, 100)) }}
+  {{ markdown(Str::limit($project->description, 200)) }}
 </div>
