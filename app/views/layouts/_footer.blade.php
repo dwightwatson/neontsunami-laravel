cdc667db09dbc5a3e3ed4ba107ff5a0ac7ef48da
Dwight Watson <dwightwatson@me.com>
2014-09-10 14:44:45 +1000

    Reduce the amount of Bootstrap used and only use scripts on the admin panel

--- app/views/layouts/_footer.blade.php
+++ app/views/layouts/_footer.blade.php
@@ -1,7 +1,9 @@
       </div>
     </div>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script type="text/javascript" src="{{ asset('assets/application.js') }}"></script>
+    @if(Active::route('admin.*'))
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+      <script type="text/javascript" src="{{ asset('assets/application.js') }}"></script>
+    @endif
     <script type="text/javascript" src="//use.typekit.net/vof3tiu.js"></script>
     <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
   </body>
