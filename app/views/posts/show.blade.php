8d52ec1a17306408fbcb94e9064e1b82c43653ad
Dwight Watson <dwightwatson@me.com>
2015-03-09 17:41:07 +1100

    Add Disqus comments to posts

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -23,5 +23,21 @@
     @endif
   </div>
 
+  <div class="comments">
+    <div id="disqus_thread"></div>
+    <script type="text/javascript">
+        /* * * CONFIGURATION VARIABLES * * */
+        var disqus_shortname = 'neontsunami';
+
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+  </div>
+
   {{ link_to_route('posts.index', '&larr; See more posts') }}
 @stop
