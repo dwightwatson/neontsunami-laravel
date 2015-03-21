ed9c15e115697f7ae6a33e3e05dfce2e080851e8
Dwight Watson <dwightwatson@me.com>
2015-03-21 05:03:20 +0000

    Add read more link to the homepage

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,6 +4,8 @@
   <h3>Recent posts</h3>
   @include('posts._post', ['post' => $latestPost])
 
+  <a href="{!! route('posts.index') !!}">Read more recent posts <span class="glyphicon glyphicon-chevron-right"></span></a>
+
   <h4>Most popular posts</h4>
   @each('posts._post', $popularPosts, 'post')
 
