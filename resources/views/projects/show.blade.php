73ad129d9e85a7ce5017c7980f4da3dc70ace129
Dwight Watson <dwightwatson@me.com>
2015-04-08 00:29:47 +1000

    Use Markdown on the project show page

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <h3>{{ $project->name }}</h3>
-  <p class="lead">{{ $project->description }}</p>
+  <p class="lead">{!! markdown($project->description) !!}</p>
 
   <a href="{{ $project->url }}" title="{{ $project->name }}">See this project <span class="glyphicon glyphicon-chevron-right"></span></a>
 @stop
