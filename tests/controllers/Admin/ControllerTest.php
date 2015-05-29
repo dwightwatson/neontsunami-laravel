a720ad0cc63fe202c69166478d98e4aed766e33f
Dwight Watson <dwightwatson@me.com>
2015-05-29 14:31:22 +1000

    Dump the Symphony failed response

--- tests/controllers/Admin/ControllerTest.php
+++ tests/controllers/Admin/ControllerTest.php
@@ -18,7 +18,7 @@ class ControllerTest extends \TestCase {
 
         $this->call('GET', 'admin');
 
-        var_dump($this->response->getOriginalContent());
+        var_dump($this->response->getContent());
 
         $this->assertResponseOk();
     }
