9ab4c5ffb84b068f0cc5da2fff12e106423c4002
Dwight Watson <dwightwatson@me.com>
2015-08-25 10:27:44 +1000

    Fix a syntax error

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -35,7 +35,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next)
     {
         if ($this->auth->check()) {
-            return new redirect()->route('home');
+            return redirect()->route('home');
         }
 
         return $next($request);
