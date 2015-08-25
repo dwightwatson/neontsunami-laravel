7f0c82718844fc5b75d4294ba041ae4ea2d8974e
Dwight Watson <dwightwatson@me.com>
2015-08-25 10:28:53 +1000

    StyleCI doesn't like my beautiful route indentation

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,8 +6,8 @@
 |--------------------------------------------------------------------------
 */
 Route::get('post/{postSlug}', 'RedirectsController@getPost');
-Route::get('tag/{tagSlug}',   'RedirectsController@getTag');
-Route::get('archive',         'RedirectsController@getArchive');
+Route::get('tag/{tagSlug}', 'RedirectsController@getTag');
+Route::get('archive', 'RedirectsController@getArchive');
 
 Route::get('tags/mac%20os%20x', 'RedirectsController@getTagsMacOsX');
 Route::get('tags/ruby%20on%20rails', 'RedirectsController@getTagsRubyOnRails');
@@ -20,8 +20,8 @@ Route::get('tags/ruby%20on%20rails', 'RedirectsController@getTagsRubyOnRails');
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
     Route::get('/', ['as' => 'admin.pages.index', 'uses' => 'PagesController@index']);
 
-    Route::get('login',     ['as' => 'admin.sessions.create', 'uses' => 'SessionsController@create']);
-    Route::post('login',    ['as' => 'admin.sessions.store', 'uses' => 'SessionsController@store']);
+    Route::get('login', ['as' => 'admin.sessions.create', 'uses' => 'SessionsController@create']);
+    Route::post('login', ['as' => 'admin.sessions.store', 'uses' => 'SessionsController@store']);
     Route::delete('logout', ['as' => 'admin.sessions.destroy', 'uses' => 'SessionsController@destroy']);
 
     Route::resource('posts', 'PostsController');
@@ -37,9 +37,9 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
 | Public routes
 |--------------------------------------------------------------------------
 */
-Route::get('/',       ['as' => 'pages.index', 'uses' => 'PagesController@index']);
-Route::get('about',   ['as' => 'pages.about', 'uses'=> 'PagesController@about']);
-Route::get('rss',     ['as' => 'pages.rss', 'uses' => 'PagesController@rss']);
+Route::get('/', ['as' => 'pages.index', 'uses' => 'PagesController@index']);
+Route::get('about', ['as' => 'pages.about', 'uses'=> 'PagesController@about']);
+Route::get('rss', ['as' => 'pages.rss', 'uses' => 'PagesController@rss']);
 
 Route::get('sitemap', ['as' => 'sitemaps.index', 'uses' => 'SitemapsController@index']);
 
