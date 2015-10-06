1234eb44cff7e43fdafbc4a8aa9e978bb48bc5c9
Dwight Watson <dwightwatson@me.com>
2015-10-06 16:01:01 +1100

    Open projects in a new window

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,5 +4,5 @@
   <h3>{{ $project->name }}</h3>
   <p class="lead">{!! markdown($project->description) !!}</p>
 
-  <a href="{{ $project->url }}" title="{{ $project->name }}">See this project <span class="glyphicon glyphicon-chevron-right"></span></a>
+  <a href="{{ $project->url }}" title="{{ $project->name }}" target="_blank">See this project <span class="glyphicon glyphicon-chevron-right"></span></a>
 @stop
