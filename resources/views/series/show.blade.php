6520ba2905ab1152d51f76574d2b97e4c83e44c9
Dwight Watson <dwightwatson@me.com>
2015-10-07 10:39:59 +1100

    Show the total number of posts available for a series, not the amount on the page

--- resources/views/series/show.blade.php
+++ resources/views/series/show.blade.php
@@ -7,7 +7,7 @@
 
   <p class="lead">{{ $series->description }}</p>
 
-  <h4>{{ $posts->count() }} {{ str_plural('post', $posts->count()) }} in this series</h4>
+  <h4>{{ $posts->total() }} {{ str_plural('post', $posts->total()) }} in this series</h4>
   @each('posts._post', $posts, 'post')
 
   {!! $posts->render() !!}
