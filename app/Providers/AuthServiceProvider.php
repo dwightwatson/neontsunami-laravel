37e89d5023c08563f32af2a84867ff22de72035d
Dwight Watson <dwightwatson@me.com>
2016-07-07 16:28:32 +1000

    Fix a namespace

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace NeonTsunami\Providers;
 
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
