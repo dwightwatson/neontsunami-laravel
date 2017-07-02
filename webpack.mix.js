c77d34af5a09ded33f322e1375ac6db887fdf98e
Dwight Watson <dwightwatson@me.com>
2017-07-02 10:47:06 +1000

    Upgrades

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,4 @@ const { mix } = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .js('resources/assets/js/admin/app.js', 'public/js/admin')
-   .version()
+   .version();
