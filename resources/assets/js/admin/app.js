fcdc33f3144896deb7679f8ed969ff7abc9fc3cd
Dwight Watson <dwightwatson@me.com>
2017-08-10 20:38:00 +1000

    Use Vue for the admin section

--- resources/assets/js/admin/app.js
+++ resources/assets/js/admin/app.js
@@ -1,39 +1,45 @@
-import '../bootstrap';
-import '../../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap';
-import Rails from 'rails-ujs';
 import 'selectize';
+import './bootstrap';
+import Vue from 'vue';
+import Rails from 'rails-ujs';
+import 'bootstrap-sass/assets/javascripts/bootstrap';
 
-(function($) {
-  Rails.start();
+Rails.start();
 
-  if ($('input[name=slug]').length && $('input[name=title], input[name=name]').length) {
-    $('input[name=title], input[name=name]').on('keyup', function() {
-      let slug = $(this).val()
-        .toLowerCase()
-        .replace(/[^\w ]+/g,'')
-        .replace(/ +/g,'-');
+const app = new Vue({
+  el: '#app',
 
-      $('input[name=slug]').val(slug);
-    });
-  }
+  mounted() {
 
-  $('input[name=tags]').selectize({
-    plugins: ['remove_button'],
-    persist: false,
-    preload: true,
-    valueField: 'name',
-    labelField: 'name',
-    searchField: 'name',
-    create: function(input, callback) {
-      $.post('/admin/tags', { name: input })
-        .done(response => {
-          callback({ 'value': response.name, 'text': response.name });
-        });
-    },
-    load: function(query, callback) {
-      $.getJSON('/admin/tags', { q: query }, response => callback(response));
+    if ($('input[name=slug]').length && $('input[name=title], input[name=name]').length) {
+      $('input[name=title], input[name=name]').on('keyup', function() {
+        let slug = $(this).val()
+          .toLowerCase()
+          .replace(/[^\w ]+/g,'')
+          .replace(/ +/g,'-');
+
+        $('input[name=slug]').val(slug);
+      });
     }
-  });
 
-  $('select[name=series_id]').selectize();
-})(jQuery);
+    $('input[name=tags]').selectize({
+      plugins: ['remove_button'],
+      persist: false,
+      preload: true,
+      valueField: 'name',
+      labelField: 'name',
+      searchField: 'name',
+      create: function(input, callback) {
+        $.post('/admin/tags', { name: input })
+          .done(response => {
+            callback({ 'value': response.name, 'text': response.name });
+          });
+      },
+      load: function(query, callback) {
+        $.getJSON('/admin/tags', { q: query }, response => callback(response));
+      }
+    });
+
+    $('select[name=series_id]').selectize();
+  }
+});
