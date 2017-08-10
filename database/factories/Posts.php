f97541b217453fc8ec3cbf4c7780c672c2f2813c
Dwight Watson <dwightwatson@me.com>
2017-08-10 20:44:20 +1000

    Add user to post factory

--- database/factories/Posts.php
+++ database/factories/Posts.php
@@ -2,6 +2,7 @@
 
 $factory->define(App\Post::class, function ($faker) {
     return [
+        'user_id'      => factory(App\User::class),
         'title'        => $faker->sentence,
         'slug'         => $faker->unique()->slug(1),
         'content'      => $faker->sentence,
