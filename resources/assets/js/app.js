6b95f0c781d28e61dd2f6743a2376b63247af916
Dwight Watson <dwightwatson@me.com>
2017-08-11 10:30:55 +1000

    Try

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,8 +2,7 @@ import Vue from 'vue';
 import Autocomplete from './components/Autocomplete.vue';
 
 const app = new Vue({
-  el: '#app',
   components: {
     Autocomplete
   }
-});
+}).$mount('#app');
