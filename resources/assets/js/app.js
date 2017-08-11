aaed49bbfdba41263ac90ebce0237d7dee747a31
Dwight Watson <dwightwatson@me.com>
2017-08-11 10:50:58 +1000

    Temporarily disable Vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import Autocomplete from './components/Autocomplete.vue';
 
-const app = new Vue({
-  el: '#app'
-});
+// const app = new Vue({
+//   el: '#app',
+
+//   components: {
+//     Autocomplete
+//   }
+// });
