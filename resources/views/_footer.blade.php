fc719cd1bbf7341e0e8887a586853057592bfec5
Dwight Watson <dwightwatson@me.com>
2018-01-01 22:15:41 +1100

    Ditch Typekit

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -2,7 +2,5 @@
     </div>
 
     <script src="{{ active('admin.*') ? mix('js/admin/app.js') : mix('js/app.js') }}"></script>
-    <script type="text/javascript" src="//use.typekit.net/vof3tiu.js"></script>
-    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
   </body>
 </html>
