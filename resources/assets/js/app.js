fb39bdca9f6f7794d010e1229f26ea69419af1ec
Dwight Watson <dwightwatson@me.com>
2018-03-22 21:44:18 +1100

    Add code highlighting with highlight.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
+import highlight from 'highlight.js';
 import Autocomplete from './components/Autocomplete.vue';
+
 
 const app = new Vue({
   el: '#app',
 
   components: {
     Autocomplete
+  },
+
+  mounted() {
+    highlight.initHighlighting();
   }
 });
